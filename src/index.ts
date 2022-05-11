c6ae09c071dae2f0f6745847ca7f5049e6070376
Samuel Male <samuelsmalek@gmail.com>
2022-05-11 15:23:35 +0300

    More exports

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,6 @@ export * from './utils/boolean-utils';
 export * from './validators/ohri-form-validator';
 export * from './utils/ohri-form-helper';
 export * from './ohri-form-context';
+export * from './components/value/view/ohri-field-value-view.component';
+export * from './components/previous-value-review/previous-value-review.component';
 export { default as OHRIForm } from './ohri-form.component';
