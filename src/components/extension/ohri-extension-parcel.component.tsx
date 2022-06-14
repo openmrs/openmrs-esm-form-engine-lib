b4c5b93dd632c415d19140313a939b41235ae861
Samuel Male <samuelsmalek@gmail.com>
2022-06-14 11:32:40 +0300

    Use an Observable as a submission notifier

--- src/components/extension/ohri-extension-parcel.component.tsx
+++ src/components/extension/ohri-extension-parcel.component.tsx
@@ -2,13 +2,18 @@ import React, { useEffect, useMemo } from 'react';
 import { OHRIFormFieldProps } from '../../api/types';
 import { attach, ExtensionSlot } from '@openmrs/esm-framework';
 import { OHRIFormContext } from '../../ohri-form-context';
+import { BehaviorSubject } from 'rxjs';
 
 const OHRIExtensionParcel: React.FC<OHRIFormFieldProps> = ({ question }) => {
   const { encounterContext, isSubmitting } = React.useContext(OHRIFormContext);
+  const submissionNotifier = useMemo(
+    () => new BehaviorSubject<{ isSubmitting: boolean }>({ isSubmitting: false }),
+    [],
+  );
 
-  const state = useMemo(() => ({ patientUuid: encounterContext.patient.id, isSubmitting }), [
-    isSubmitting,
+  const state = useMemo(() => ({ patientUuid: encounterContext.patient.id, submissionNotifier }), [
     encounterContext.patient.id,
+    submissionNotifier,
   ]);
 
   useEffect(() => {
@@ -16,6 +21,10 @@ const OHRIExtensionParcel: React.FC<OHRIFormFieldProps> = ({ question }) => {
       attach(question.questionOptions.extensionSlotName, question.questionOptions.extensionId);
     }
   }, []);
+
+  useEffect(() => {
+    submissionNotifier.next({ isSubmitting: isSubmitting });
+  }, [isSubmitting, submissionNotifier]);
 
   return (
     <>
