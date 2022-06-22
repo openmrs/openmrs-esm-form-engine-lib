be4faa031aab0dc1a61e2c440971d7ee7662125e
Samuel Male <samuelsmalek@gmail.com>
2022-06-22 20:14:30 +0300

    (feat) Use custom version comparator (#21)

--- src/utils/forms-loader.ts
+++ src/utils/forms-loader.ts
@@ -67,10 +67,8 @@ export function getLatestFormVersion(forms: FormJsonFile[]) {
   if (forms.length == 1) {
     return forms[0];
   }
-  const latest = semver.maxSatisfying(
-    forms.map(f => f.semanticVersion),
-    '*',
-  );
+  const candidates = forms.map(f => f.semanticVersion);
+  const latest = candidates.sort(formsVersionComparator)[candidates.length - 1];
   return forms.find(f => f.semanticVersion == latest);
 }
 
@@ -264,4 +262,41 @@ export function updateExcludeIntentBehaviour(excludedIntents: Array<string>, ori
 
 export function addToBaseFormsRegistry(customRegistry: Record<string, any>) {
   baseRegistry = { ...baseRegistry, ...customRegistry };
+}
+
+function isPositiveInteger(x) {
+  return /^\d+$/.test(x);
+}
+
+function formsVersionComparator(v1, v2) {
+  var v1parts = v1.split('.');
+  var v2parts = v2.split('.');
+  // First, validate both numbers are true version numbers
+  function validateParts(parts) {
+    for (var i = 0; i < parts.length; ++i) {
+      if (!isPositiveInteger(parts[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  if (!validateParts(v1parts) || !validateParts(v2parts)) {
+    return NaN;
+  }
+  for (var i = 0; i < v1parts.length; ++i) {
+    if (v2parts.length === i) {
+      return 1;
+    }
+    if (v1parts[i] === v2parts[i]) {
+      continue;
+    }
+    if (v1parts[i] > v2parts[i]) {
+      return 1;
+    }
+    return -1;
+  }
+  if (v1parts.length != v2parts.length) {
+    return -1;
+  }
+  return 0;
 }
