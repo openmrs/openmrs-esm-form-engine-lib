e3ddd1ddbec5de2bc154d386abbe25e74c078879
Samuel Male <samuelsmalek@gmail.com>
2022-07-27 18:10:22 +0300

    Fixed value types should not edit existing observations (#31)

--- src/components/inputs/fixed-value/ohri-fixed-value.component.tsx
+++ src/components/inputs/fixed-value/ohri-fixed-value.component.tsx
@@ -2,19 +2,27 @@ import React, { useEffect } from 'react';
 import { OHRIFormContext } from '../../../ohri-form-context';
 import { OHRIFormFieldProps } from '../../../api/types';
 import { isObject } from 'lodash-es';
+import { isEmpty } from '../../../validators/ohri-form-validator';
+import { isTrue } from '../../../utils/boolean-utils';
 
 const OHRIFixedValue: React.FC<OHRIFormFieldProps> = ({ question, handler }) => {
   const { encounterContext, isFieldInitializationComplete } = React.useContext(OHRIFormContext);
-
   useEffect(() => {
     if (question.value && typeof question.value == 'string' && isFieldInitializationComplete) {
       delete question.value;
       handler.handleFieldSubmission(question, question['fixedValue'], encounterContext);
     } else if (isObject(question.value) && !obsValueEqualTo(question['fixedValue'], question.value)) {
-      // edit obs
+      // fixed-value rendering types aren't supposed to EDIT an existing value
+      // by the time this point is reached, it means an Observation with
+      // the same concept question within this encounter has been assigned to this field
+      // I will just ignore that Observation
+      delete question.value;
+      handler.handleFieldSubmission(question, question['fixedValue'], encounterContext);
+    } else if (!isTrue(question.isHidden) && isEmpty(question.value)) {
       handler.handleFieldSubmission(question, question['fixedValue'], encounterContext);
     }
-  }, [question.value, isFieldInitializationComplete]);
+  }, [question.value, isFieldInitializationComplete, question.isHidden]);
+
   return <></>;
 };
 
