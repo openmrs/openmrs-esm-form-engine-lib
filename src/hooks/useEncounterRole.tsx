363f2870478b334188c3cb4f4979d43eafdd905e
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-01 14:44:14 +0300

    (feat) Add ability to retrieve encounter type and default to `unknown` encounter type

---
+++ src/hooks/useEncounterRole.tsx
@@ -0,0 +1,15 @@
+import { OpenmrsResource, openmrsFetch } from '@openmrs/esm-framework';
+import useSWRImmutable from 'swr/immutable';
+
+export function useEncounterRole() {
+  const { data, error, isLoading } = useSWRImmutable<{ data: { results: Array<OpenmrsResource> } }, Error>(
+    '/ws/rest/v1/encounterrole?v=custom:(uuid,display,name)',
+    openmrsFetch,
+  );
+  const clinicalEncounterRole = data?.data.results.find(encounterRole => encounterRole.name === 'Clinician');
+
+  if (clinicalEncounterRole) {
+    return { encounterRole: clinicalEncounterRole, error, isLoading };
+  }
+  return { encounterRole: data?.data.results[0], error, isLoading };
+}
