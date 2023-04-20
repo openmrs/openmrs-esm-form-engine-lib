cd4a9e6a193e5b155a88528ab13c3ff830571bd5
CynthiaKamau <cynthiakamau54@gmail.com>
2023-04-20 15:07:22 +0300

    O3-2061 Pass active visit attributes as default in visit related form fields (#30)

---
+++ __mocks__/visit.mock.ts
@@ -0,0 +1,19 @@
+export const mockVisit =
+{
+  uuid: '15dd49ba-4283-472f-bce3-05401f85c0d3',
+  patient: {
+    uuid: '5a4e7a05-e275-4c14-acab-cb86f3e16353',
+    display: '102EWH - Test Patient Registration',
+  },
+  visitType: {
+    uuid: '7b0f5697-27e3-40c4-8bae-f4049abfb4ed',
+    display: 'Facility Visit',
+  },
+  location: {
+    uuid: '7fdfa2cb-bc95-405a-88c6-32b7673c0453',
+    display: 'Laboratory',
+  },
+  startDatetime: '2020-07-28T10:29:00.000+0000',
+  stopDatetime: '2020-07-29T10:29:00.000+0000',
+  encounters: [],
+}
