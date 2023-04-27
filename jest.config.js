ec41829ff3f33fe8b880712b638aa7c4ef45629a
Dennis Kigen <kigen.work@gmail.com>
2023-04-27 19:45:24 +0300

    (refactor) Remove unused styles and style imports + add eslint rules

--- jest.config.js
+++ jest.config.js
@@ -16,8 +16,6 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!@openmrs)'],
   moduleNameMapper: {
     '\\.(s?css)$': 'identity-obj-proxy',
-    '^@carbon/icons-react/es/(.*)$': '@carbon/icons-react/lib/$1',
-    '^carbon-components-react/es/(.*)$': 'carbon-components-react/lib/$1',
     '@openmrs/esm-framework': '@openmrs/esm-framework/mock',
     'react-i18next': '<rootDir>/__mocks__/react-i18next.js',
     'lodash-es': 'lodash',
