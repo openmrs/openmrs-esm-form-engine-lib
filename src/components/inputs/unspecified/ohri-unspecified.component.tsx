32206d82ac3674373ce619b2934ee8775a05d9a5
kajambiya <jssebaggala@outlook.com>
2023-05-10 14:36:59 +0300

    add skip logic to answer options

--- src/components/inputs/unspecified/ohri-unspecified.component.tsx
+++ src/components/inputs/unspecified/ohri-unspecified.component.tsx
@@ -3,17 +3,17 @@ import { Checkbox } from '@carbon/react';
 import { useField } from 'formik';
 import { OHRIFormContext } from '../../../ohri-form-context';
 import { OHRIFieldValidator } from '../../../validators/ohri-form-validator';
-import { OHRIFormField } from '../../../api/types';
+import { OHRIFormFieldProps, SessionMode } from '../../../api/types';
 import { isTrue } from '../../../utils/boolean-utils';
 import styles from './ohri-unspecified.scss';
 
-export const OHRIUnspecified: React.FC<{
-  question: OHRIFormField;
-}> = ({ question }) => {
+export const OHRIUnspecified: React.FC<OHRIFormFieldProps> = ({ question, onChange, handler }) => {
   const [field, meta] = useField(`${question.id}-unspecified`);
   const { setFieldValue, encounterContext } = React.useContext(OHRIFormContext);
   const [previouslyUnspecified, setPreviouslyUnspecified] = useState(false);
   const hideCheckBox = encounterContext.sessionMode == 'view';
+  const [errors, setErrors] = useState([]);
+  const [warnings, setWarnings] = useState([]);
 
   useEffect(() => {
     if (field.value) {
@@ -42,6 +42,13 @@ export const OHRIUnspecified: React.FC<{
   }, [field.value]);
 
   useEffect(() => {
+    if (question['submission']) {
+      question['submission'].errors && setErrors(question['submission'].errors);
+      question['submission'].warnings && setWarnings(question['submission'].warnings);
+    }
+  }, [question['submission']]);
+
+  useEffect(() => {
     if (question.value) {
       setFieldValue(`${question.id}-unspecified`, false);
     }
@@ -49,6 +56,8 @@ export const OHRIUnspecified: React.FC<{
 
   const handleOnChange = useCallback(value => {
     setFieldValue(`${question.id}-unspecified`, value.target.checked);
+    onChange(question.id, field.value, setErrors, setWarnings);
+    question.value = handler?.handleFieldSubmission(question, field.value, encounterContext);
   }, []);
 
   return (
