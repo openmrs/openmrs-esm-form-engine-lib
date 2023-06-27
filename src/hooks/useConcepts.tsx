b8a9d7503409c3272708c9b754c1a4d267dcd6d0
JOSH <sibendirejoshua@gmail.com>
2023-06-27 14:21:58 +0300

    Update useConcepts hook to fetch all referenced concepts with pagination

--- src/hooks/useConcepts.tsx
+++ src/hooks/useConcepts.tsx
@@ -5,10 +5,30 @@ const conceptRepresentation =
   'custom:(uuid,display,conceptMappings:(conceptReferenceTerm:(conceptSource:(name),code)))';
 
 export function useConcepts(references: Set<string>) {
+  const pageSize = 50;
+
+  const fetchConcepts = async (url:String) => {
+    const response = await openmrsFetch(url);
+    const totalCount = response.headers.get('X-Total-Count');
+    const totalPages = Math.ceil(Number(totalCount)/pageSize);
+
+    const results: OpenmrsResource[] = await response.json();
+    let fetchedResults = results;
+
+    for (let page = 2; page <= totalPages; page++) {
+      const nextPageUrl = `${url}&startIndex=${(page - 1) * pageSize}&v=${conceptRepresentation}`;
+      const nextPageResponse = await openmrsFetch(nextPageUrl);
+      const nextPageResults: OpenmrsResource[] = await nextPageResponse.json();
+      fetchedResults = fetchedResults.concat(nextPageResults);
+    }
+
+    return { results: fetchedResults };
+  };
+  
   // TODO: handle paging (ie when number of concepts greater than default limit per page)
   const { data, error, isLoading } = useSWRImmutable<{ data: { results: Array<OpenmrsResource> } }, Error>(
     `/ws/rest/v1/concept?references=${Array.from(references).join(',')}&v=${conceptRepresentation}`,
     openmrsFetch,
   );
   return { concepts: data?.data.results, error, isLoading };
-}
+  }
