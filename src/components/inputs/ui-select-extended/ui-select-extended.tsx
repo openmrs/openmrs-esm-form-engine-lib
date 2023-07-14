90930db0631d6da8c7c11fc14a18c7976c67d61a
kajambiya <jssebaggala@outlook.com>
2023-07-14 04:14:57 -0500

    fix loader and data binding

--- src/components/inputs/ui-select-extended/ui-select-extended.tsx
+++ src/components/inputs/ui-select-extended/ui-select-extended.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { ComboBox } from '@carbon/react';
+import { ComboBox, InlineLoading } from '@carbon/react';
 import { OHRIFormFieldProps } from '../../../api/types';
 import { useField } from 'formik';
 import styles from './ui-select-extended.scss';
@@ -11,9 +11,10 @@ import { getDataSource } from '../../../registry/registry';
 import { fieldRequiredErrCode, isEmpty } from '../../../validators/ohri-form-validator';
 import { PreviousValueReview } from '../../previous-value-review/previous-value-review.component';
 import debounce from 'lodash-es/debounce';
-import InlineLoader from '../../loaders/inline-loader.component';
+import { useTranslation } from 'react-i18next';
 
 export const UISelectExtended: React.FC<OHRIFormFieldProps> = ({ question, handler, onChange }) => {
+  const { t } = useTranslation();
   const [field, meta] = useField(question.id);
   const { setFieldValue, encounterContext, fields } = React.useContext(OHRIFormContext);
   const [conceptName, setConceptName] = useState('Loading...');
@@ -22,6 +23,7 @@ export const UISelectExtended: React.FC<OHRIFormFieldProps> = ({ question, handl
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
   const isFieldRequiredError = useMemo(() => errors[0]?.errCode == fieldRequiredErrCode, [errors]);
   const [previousValueForReview, setPreviousValueForReview] = useState(null);
   const inputValue = useRef('');
@@ -62,7 +64,6 @@ export const UISelectExtended: React.FC<OHRIFormFieldProps> = ({ question, handl
     // get the data source
     if (dataSource && isTrue(question.questionOptions['isSearchable']) && !isEmpty(searchTerm)) {
       debouncedSearch(searchTerm, dataSource);
-    } else {
     }
   }, [dataSource, searchTerm]);
 
@@ -89,66 +90,66 @@ export const UISelectExtended: React.FC<OHRIFormFieldProps> = ({ question, handl
     }
     return changes;
   };
-
   return encounterContext.sessionMode == 'view' || isTrue(question.readonly) ? (
     <div className={styles.formField}>
       <OHRIFieldValueView
         label={question.label}
-        value={field.value ? field.value.display : field.value}
+        value={
+          field.value
+            ? handler?.getDisplayValue(question, items.find(item => item.uuid == field.value)?.display)
+            : field.value
+        }
         conceptName={conceptName}
         isInline
       />
     </div>
   ) : (
     !question.isHidden && (
-      <div className={`${styles.formInputField} ${styles.row}`}>
-        <div
-          className={
-            isFieldRequiredError
-              ? `${styles.errorLabel} ${styles.multiselectOverride}`
-              : `${styles.multiselectOverride}`
-          }>
-          <ComboBox
-            id={question.id}
-            titleText={question.label}
-            items={items}
-            isLoading={isLoading}
-            loadingMessage="loading..."
-            itemToString={item => item?.display}
-            selectedItem={field.value}
-            shouldFilterItem={({ item, inputValue }) => {
-              if (!inputValue) {
-                // Carbon's initial call at component mount
-                return true;
-              }
-              return item.display.toLowerCase().includes(inputValue.toLowerCase());
-            }}
-            onChange={({ selectedItem }) => handleChange(selectedItem)}
-            disabled={question.disabled}
-            onInputChange={value => {
-              inputValue.current = value;
-              if (question.questionOptions['isSearchable']) {
-                setSearchTerm(value);
-              }
-            }}
-          />
-        </div>
-        {isLoading ? (
-          <div>
-            <InlineLoader />
+      <>
+        <div className={`${styles.formInputField} ${styles.row}`}>
+          <div
+            className={
+              isFieldRequiredError
+                ? `${styles.errorLabel} ${styles.multiselectOverride}`
+                : `${styles.multiselectOverride}`
+            }>
+            <ComboBox
+              id={question.id}
+              titleText={question.label}
+              items={items}
+              isLoading={isLoading}
+              loadingMessage="loading..."
+              itemToString={item => item?.display}
+              selectedItem={items.find(item => item.uuid == field.value)}
+              shouldFilterItem={({ item, inputValue }) => {
+                if (!inputValue) {
+                  // Carbon's initial call at component mount
+                  return true;
+                }
+                return item.display.toLowerCase().includes(inputValue.toLowerCase());
+              }}
+              onChange={({ selectedItem }) => handleChange(selectedItem?.uuid)}
+              disabled={question.disabled}
+              onInputChange={value => {
+                inputValue.current = value;
+                if (question.questionOptions['isSearchable']) {
+                  setSearchTerm(value);
+                }
+              }}
+            />
           </div>
-        ) : (
-          previousValueForReview && (
+          {previousValueForReview && (
             <div>
               <PreviousValueReview
                 value={previousValueForReview.value}
-                displayText={previousValueForReview.display}
+                displayText={items.find(item => item.uuid == previousValueForReview.value)?.display}
                 setValue={handleChange}
               />
             </div>
-          )
-        )}
-      </div>
+          )}
+        </div>
+        {isLoading && <InlineLoading className={styles.loader} description={t('loading', 'Loading') + '...'} />}
+      </>
     )
   );
 };
