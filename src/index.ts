b8dc67e0640bd5e641c617c69c37b1724428ded1
arodidev <arodi745@gmail.com>
2023-09-06 23:44:20 +0300

    export validator hook

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ export * from './ohri-form-context';
 export * from './components/value/view/ohri-field-value-view.component';
 export * from './components/previous-value-review/previous-value-review.component';
 export * from './hooks/useFormJson';
-export * from './validators/schema-validator';
+export * from './hooks/useSchemaValidator';
 export { default as OHRIForm } from './ohri-form.component';
