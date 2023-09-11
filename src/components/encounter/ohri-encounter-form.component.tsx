d34941b46cb3b54843b35f7956a12e6815f5218a
arodidev <arodi745@gmail.com>
2023-09-11 15:05:42 +0300

    clean up

--- src/components/encounter/ohri-encounter-form.component.tsx
+++ src/components/encounter/ohri-encounter-form.component.tsx
@@ -120,7 +120,7 @@ export const OHRIEncounterForm: React.FC<OHRIEncounterFormProps> = ({
           evaluateFieldReadonlyProp(question, section.readonly, page.readonly, form.readonly);
           if (question.questionOptions.rendering == 'fixed-value' && !question['fixedValue']) {
             question['fixedValue'] = question.value;
-            }
+          }
           flattenedFieldsTemp.push(question);
           if (question.type == 'obsGroup') {
             question.questions.forEach(groupedField => {
@@ -402,7 +402,7 @@ export const OHRIEncounterForm: React.FC<OHRIEncounterFormProps> = ({
       .filter(field => field.value || field.type == 'obsGroup') // filter out fields with empty values except groups
       .filter(field => !field.isParentHidden && !field.isHidden && (field.type == 'obs' || field.type == 'obsGroup'))
       .filter(field => !field['groupId']) // filter out grouped obs
-      .filter(field => field.questionOptions['isSubmittable'])
+      .filter(field => field.questionOptions.isSubmittable)
       .forEach(field => {
         if (field.type == 'obsGroup') {
           const obsGroup = {
