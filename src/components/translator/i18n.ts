06832cb4854ed246efc2dfcd2a0b8e77029664a6
LarsLemos <larslemos@gmail.com>
2023-09-28 08:46:36 +0200

    Redefine i18 ohri namespace

--- src/components/translator/i18n.ts
+++ src/components/translator/i18n.ts
@@ -1,26 +1,27 @@
-import { UseTranslationOptions, initReactI18next } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
-
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Import your language files
 import ptTranslation from './pt.json';
 import frTranslation from './fr.json';
 import enTranslation from './en.json';
 
-i18next.use(initReactI18next).init({
-  debug: true,
-  fallbackLng: ['pt', 'fr', 'en'], 
-  resources: {
-    pt: {
-      ohri: ptTranslation,
-    },
-    fr: {
-      ohri: frTranslation,
-    },
-    em: {
-      ohri: enTranslation,
-    },
-  },
-});
+// Define supported languages and their resources
+const supportedLanguages = ['pt', 'fr', 'en'];
+const resources = {
+  pt: { ohri: ptTranslation },
+  fr: { ohri: frTranslation },
+  en: { ohri: enTranslation },
+};
+
+i18next
+  .use(LanguageDetector) // Enable Browser language detection
+  .use(initReactI18next) // Initialize React integration
+  .init({
+    debug: true,
+    fallbackLng: supportedLanguages, // Fallback to supported languages
+    resources,
+  });
 
 export default i18next;
