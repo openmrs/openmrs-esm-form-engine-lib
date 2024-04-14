cc2a7e4951e7d728fb05b90192a735bce42c2660
arodidev <51090527+arodidev@users.noreply.github.com>
2024-04-14 21:07:04 +0300

    bug fix for date previous-value display

--- src/components/section/helpers.ts
+++ src/components/section/helpers.ts
@@ -46,7 +46,7 @@ function previousValueDisplayForCheckbox(previosValueItems: Object[]): String {
 export const formatPreviousValueDisplayText = (question: OHRIFormField, value: any) => {
   switch (question.questionOptions.rendering) {
     case 'date':
-      return formatDate(value);
+      return formatDate(new Date(value?.display));
     case 'checkbox':
       return Array.isArray(value) ? previousValueDisplayForCheckbox(value) : null;
     default:
