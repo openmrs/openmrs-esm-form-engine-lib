6cb98daa50e146503bfe88cc551d40944e6cf5c0
reagan-meant <reaganmeant@gmail.com>
2024-04-16 12:55:45 +0300

    O3-1990: Allow string as value for markdown

--- src/components/inputs/markdown/ohri-markdown.component.tsx
+++ src/components/inputs/markdown/ohri-markdown.component.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { OHRIFormFieldProps } from '../../../api/types';
 
-const OHRIMarkdown: React.FC<OHRIFormFieldProps> = ({ question }) =>
-  !question.isHidden && <ReactMarkdown children={question.value.join('\n')} />;
-
+const OHRIMarkdown: React.FC<OHRIFormFieldProps> = ({ question }) =>{
+  if (!question.isHidden) {
+    if (typeof question.value === 'string') {
+      return <ReactMarkdown>{question.value}</ReactMarkdown>;
+    } else if (Array.isArray(question.value)) {
+      return <ReactMarkdown children={question.value.join('\n')} />;
+    } else {
+      return <div>Invalid value type to render</div>;
+    }
+  }
+};
 export default OHRIMarkdown;
