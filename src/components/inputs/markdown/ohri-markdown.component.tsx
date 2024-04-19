f449232f1946d030e4a73eb395996becc9772056
reagan-meant <reaganmeant@gmail.com>
2024-04-19 12:46:49 +0300

    Limited the markdown options

--- src/components/inputs/markdown/ohri-markdown.component.tsx
+++ src/components/inputs/markdown/ohri-markdown.component.tsx
@@ -3,14 +3,8 @@ import ReactMarkdown from 'react-markdown';
 import { OHRIFormFieldProps } from '../../../api/types';
 
 const OHRIMarkdown: React.FC<OHRIFormFieldProps> = ({ question }) =>{
-  if (!question.isHidden) {
-    if (typeof question.value === 'string') {
-      return <ReactMarkdown>{question.value}</ReactMarkdown>;
-    } else if (Array.isArray(question.value)) {
-      return <ReactMarkdown children={question.value.join('\n')} />;
-    } else {
-      return <div>Invalid value type to render</div>;
-    }
-  }
+  const markdownTypesAllowed = ['h1', 'h2', 'h3','h4', 'h5', 'h6', 'p', 'strong', 'em'];
+  const markdownValue = Array.isArray(question.value) ? question.value.join('\n') : question.value;
+  return !question.isHidden && <ReactMarkdown children={markdownValue} unwrapDisallowed={true} allowedElements={markdownTypesAllowed}/>;
 };
 export default OHRIMarkdown;
