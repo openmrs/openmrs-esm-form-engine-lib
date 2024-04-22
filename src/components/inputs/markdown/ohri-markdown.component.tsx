affc9bdaf5b4170be018125cf94749a05394ed96
reagan-meant <reaganmeant@gmail.com>
2024-04-22 11:18:35 +0300

    Refactor code

--- src/components/inputs/markdown/ohri-markdown.component.tsx
+++ src/components/inputs/markdown/ohri-markdown.component.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 import { OHRIFormFieldProps } from '../../../api/types';
+import  MarkdownWrapper  from './markdown-wrapper.component';
 
 const OHRIMarkdown: React.FC<OHRIFormFieldProps> = ({ question }) =>{
-  const markdownTypesAllowed = ['h1', 'h2', 'h3','h4', 'h5', 'h6', 'p', 'strong', 'em'];
-  const markdownValue = Array.isArray(question.value) ? question.value.join('\n') : question.value;
-  return !question.isHidden && <ReactMarkdown children={markdownValue} unwrapDisallowed={true} allowedElements={markdownTypesAllowed}/>;
+  return !question.isHidden && <MarkdownWrapper markdown={question.value} />;
 };
 export default OHRIMarkdown;
--- src/ohri-form.component.tsx
+++ src/ohri-form.component.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
 import { Form, Formik } from 'formik';
 import { Button, ButtonSet, InlineLoading } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
@@ -29,6 +28,7 @@ import OHRIFormSidebar from './components/sidebar/ohri-form-sidebar.component';
 import WarningModal from './components/warning-modal.component';
 import styles from './ohri-form.component.scss';
 import { evaluatePostSubmissionExpression } from './utils/post-submission-action-helper';
+import MarkdownWrapper from './components/inputs/markdown/markdown-wrapper.component';
 
 interface OHRIFormProps {
   patientUUID: string;
@@ -319,11 +319,7 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
                     {showPatientBanner && <PatientBanner patient={patient} hideActionsOverflow={true} />}
                     {refinedFormJson.markdown && (
                       <div className={styles.markdownContainer}>
-                        <ReactMarkdown
-                          children={Array.isArray(refinedFormJson.markdown) ? refinedFormJson.markdown.join('\n') : refinedFormJson.markdown}
-                          unwrapDisallowed={true}
-                          allowedElements={['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'strong', 'em']}
-                        />
+                        <MarkdownWrapper markdown={refinedFormJson.markdown} />
                       </div>
                     )}
                     <div
