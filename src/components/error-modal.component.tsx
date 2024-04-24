1c8525d78072a6db99ef26b539302bed23a3d361
reagan-meant <reaganmeant@gmail.com>
2024-04-24 19:50:32 +0300

    Add the inline notification for errors

---
+++ src/components/error-modal.component.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  InlineNotification,
+} from '@carbon/react';
+import { useTranslation } from 'react-i18next';
+
+const ErrorModal: React.FC<{error: Error}> = ({ error }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+     <InlineNotification
+              style={{ minWidth: '100%' }}
+              kind="error"
+              lowContrast={true}
+              hideCloseButton={false}
+              title= {t('errorDescriptionTitle', 'Error')}
+              subtitle={t('errorDescription', error.message)}
+            />
+            
+    </>
+  );
+};
+
+export default ErrorModal;
