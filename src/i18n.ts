fadd525d686d9a1710138effdca7dd92c8662592
Vineet Sharma <sharmava05@gmail.com>
2024-04-24 17:03:46 +0300

    Load translation resources in i18next on loading

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,39 +1,11 @@
-import * as i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+function setupFormEngineLibI18n() {
+  const lang = window.i18next.language;
+  import(`../translations/${lang}.json`)
+    .then((json) => {
+      const data = json ?? {};
+      window.i18next.addResourceBundle(lang, '@openmrs/openmrs-form-engine-lib', data, true, true);
+    })
+    .catch((err) => console.error(err));
+}
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
-
-window.formEnginei18next = i18n.default;
-
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .use<i18n.BackendModule>({
-    type: 'backend',
-    init() {},
-    read: (language, namespace, callback) => {
-      import(`../translations/${language}.json`)
-        .then((json) => {
-          const data = json ?? {};
-          callback(null, data);
-        })
-        .catch((err) => {
-          callback(err, null);
-        });
-    },
-  })
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    ns: '@openmrs/esm-form-engine-lib',
-    detection: {
-      order: ['querystring', 'htmlTag', 'localStorage', 'navigator'],
-      lookupQuerystring: 'lang',
-    },
-  });
-
-export default i18n;
+setupFormEngineLibI18n();
--- src/withTranslation.tsx
+++ src/withTranslation.tsx
@@ -4,7 +4,7 @@ import { I18nextProvider } from 'react-i18next';
 export default function withTranslation(WrappedComponent: React.ComponentType<any>) {
   return function WithTranslation(props: any) {
     return (
-      <I18nextProvider i18n={window.formEnginei18next}>
+      <I18nextProvider i18n={window.i18next} defaultNS="@openmrs/openmrs-form-engine-lib">
         <WrappedComponent {...props} />
       </I18nextProvider>
     );
