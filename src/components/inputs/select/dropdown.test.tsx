654fe597f0418d9802fed1917a8d9bdecacba3fd
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-04-24 18:06:17 +0300

    chore: add unit tests

--- src/components/inputs/select/dropdown.test.tsx
+++ src/components/inputs/select/dropdown.test.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
 import { render, fireEvent, screen, cleanup, act } from '@testing-library/react';
-import { Formik } from 'formik';
-import { type EncounterContext, FormContext } from '../../../form-context';
+import { Form, Formik } from 'formik';
+import { EncounterContext, FormContext } from '../../../form-context';
 import Dropdown from './dropdown.component';
-import { type FormField } from '../../../types';
+import { FormField } from '../../../types';
 import { ObsSubmissionHandler } from '../../../submission-handlers/base-handlers';
+import { openmrsFetch } from '@openmrs/esm-framework';
 
-const question: FormField = {
-  label: 'Patient past program.',
-  type: 'obs',
-  questionOptions: {
-    rendering: 'select',
-    concept: '1c43b05b-b6d8-4eb5-8f37-0b14f5347568',
-    answers: [
-      {
-        label: 'HIV Care and Treatment',
-        value: '6ddd933a-e65c-4f35-8884-c555b50c55e1',
-      },
-      {
-        label: 'Oncology Screening and Diagnosis Program',
-        value: '12f7be3d-fb5d-47dc-b5e3-56c501be80a6',
-      },
-      {
-        label: 'Fight Malaria Initiative',
-        value: '14cd2628-8a33-4b93-9c10-43989950bba0',
-      },
-    ],
+const mockedOpenmrsFetch = openmrsFetch as jest.Mock;
+
+const questions: FormField[] =  [
+  {
+    label: 'Patient past program.',
+    type: 'obs',
+    questionOptions: {
+      rendering: 'select',
+      concept: '1c43b05b-b6d8-4eb5-8f37-0b14f5347568',
+      answers: [
+        {
+          label: 'HIV Care and Treatment',
+          value: '6ddd933a-e65c-4f35-8884-c555b50c55e1',
+        },
+        {
+          label: 'Oncology Screening and Diagnosis Program',
+          value: '12f7be3d-fb5d-47dc-b5e3-56c501be80a6',
+        },
+        {
+          label: 'Fight Malaria Initiative',
+          value: '14cd2628-8a33-4b93-9c10-43989950bba0',
+        },
+      ],
+    },
+    value: null,
+    id: 'patient-past-program',
   },
-  value: null,
-  id: 'patient-past-program',
-};
+  {
+    label: 'Select criteria for new WHO stage:',
+    type: 'obs',
+    questionOptions: {
+      concept: '250e87b6-beb7-44a1-93a1-d3dd74d7e372',
+      rendering: 'select-concept-answers' as unknown as RenderType,
+    },
+    validators: [],
+    id: '__sq5ELJr7p',
+  },
+];
 
 const encounterContext: EncounterContext = {
   patient: {
@@ -50,34 +65,53 @@ const encounterContext: EncounterContext = {
   setEncounterLocation: jest.fn,
 };
 
-const renderForm = (initialValues) => {
+const renderForm = (intialValues) => {
   render(
-    <Formik initialValues={initialValues} onSubmit={null}>
+    <Formik initialValues={intialValues} onSubmit={null}>
       {(props) => (
-        <FormContext.Provider
-          value={{
-            values: props.values,
-            setFieldValue: props.setFieldValue,
-            setEncounterLocation: jest.fn(),
-            obsGroupsToVoid: [],
-            setObsGroupsToVoid: jest.fn(),
-            encounterContext: encounterContext,
-            fields: [question],
-            isFieldInitializationComplete: true,
-            isSubmitting: false,
-            formFieldHandlers: { obs: ObsSubmissionHandler },
-          }}>
-          <Dropdown question={question} onChange={jest.fn()} handler={ObsSubmissionHandler} />
-        </FormContext.Provider>
+        <Form>
+          <FormContext.Provider
+            value={{
+              values: props.values,
+              setFieldValue: props.setFieldValue,
+              setEncounterLocation: jest.fn(),
+              obsGroupsToVoid: [],
+              setObsGroupsToVoid: jest.fn(),
+              encounterContext: encounterContext,
+              fields: questions,
+              isFieldInitializationComplete: true,
+              isSubmitting: false,
+              formFieldHandlers: { obs: ObsSubmissionHandler },
+            }}>
+            <Dropdown question={questions[0]} onChange={jest.fn()} handler={ObsSubmissionHandler} />
+            <Dropdown question={questions[1]} onChange={jest.fn()} handler={ObsSubmissionHandler} />
+          </FormContext.Provider>
+        </Form>
       )}
     </Formik>,
   );
 };
 
+jest.mock('../../../registry/registry', () => ({
+  getRegisteredDataSource: jest.fn().mockResolvedValue({
+    fetchData: jest.fn().mockResolvedValue([
+      {
+        uuid: 'stage-1-uuid',
+        display: 'stage 1',
+      },
+      {
+        uuid: 'stage-2-uuid',
+        display: 'stage 2',
+      },
+    ]),
+    toUuidAndDisplay: (data) => data,
+  }),
+}));
+
 describe('dropdown input field', () => {
   afterEach(() => {
     // teardown
-    question.value = null;
+    questions[0].value = null;
   });
 
   it('should record new obs', async () => {
@@ -87,7 +121,7 @@ describe('dropdown input field', () => {
 
     // assert initial values
     await act(async () => {
-      expect(question.value).toBe(null);
+      expect(questions[0].value).toBe(null);
     });
 
     // choose an option
@@ -97,7 +131,7 @@ describe('dropdown input field', () => {
 
     // verify
     await act(async () => {
-      expect(question.value).toEqual({
+      expect(questions[0].value).toEqual({
         person: '833db896-c1f0-11eb-8529-0242ac130003',
         obsDatetime: new Date(2020, 11, 29),
         concept: '1c43b05b-b6d8-4eb5-8f37-0b14f5347568',
@@ -114,7 +148,7 @@ describe('dropdown input field', () => {
 
   it('should edit obs', async () => {
     // setup
-    question.value = {
+    questions[0].value = {
       uuid: '305ed1fc-c1fd-11eb-8529-0242ac130003',
       person: '833db896-c1f0-11eb-8529-0242ac130003',
       obsDatetime: encounterContext.encounterDate,
@@ -125,7 +159,7 @@ describe('dropdown input field', () => {
       voided: false,
       value: '6ddd933a-e65c-4f35-8884-c555b50c55e1',
     };
-    await renderForm({ 'patient-past-program': question.value.value });
+    await renderForm({ 'patient-past-program': questions[0].value.value });
     const dropdownWidget = screen.getByRole('combobox', { name: /Patient past program./ });
 
     // do some edits
@@ -135,7 +169,7 @@ describe('dropdown input field', () => {
 
     // verify
     await act(async () => {
-      expect(question.value).toEqual({
+      expect(questions[0].value).toEqual({
         uuid: '305ed1fc-c1fd-11eb-8529-0242ac130003',
         person: '833db896-c1f0-11eb-8529-0242ac130003',
         obsDatetime: new Date(2020, 11, 29),
@@ -147,5 +181,18 @@ describe('dropdown input field', () => {
         value: '12f7be3d-fb5d-47dc-b5e3-56c501be80a6',
       });
     });
+  });
+
+  it('renders items from the datasource', async () => {
+    await act(async () => {
+      await renderForm({});
+    });
+
+    const dropdownWidget = screen.getByRole('combobox', { name: /Select criteria for new WHO stage:/i });
+    fireEvent.click(dropdownWidget);
+
+    // Assert that all items are displayed
+    expect(screen.getByText('stage 1')).toBeInTheDocument();
+    expect(screen.getByText('stage 2')).toBeInTheDocument();
   });
 });
