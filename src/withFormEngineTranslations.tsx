709eca19bcd0c71334cbefb23c209e9f87a1d594
Vineet Sharma <sharmava05@gmail.com>
2024-04-24 20:35:13 +0300

    Updated withFormEngineTranslations

--- src/withFormEngineTranslations.tsx
+++ src/withFormEngineTranslations.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { moduleName } from './globals';
 
 export default function withFormEngineTranslations(WrappedComponent: React.ComponentType<any>) {
   return function WithTranslation(props: any) {
     return (
-      <I18nextProvider i18n={window.i18next} defaultNS="@openmrs/openmrs-form-engine-lib">
+      <I18nextProvider i18n={window.i18next} defaultNS={moduleName}>
         <WrappedComponent {...props} />
       </I18nextProvider>
     );
