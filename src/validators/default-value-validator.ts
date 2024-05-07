ba8de852179d4209febe81ac148c8a4de8f365d8
gitcliff <gitacliff48@gmail.com>
2024-05-07 09:45:00 +0300

    field-validation:adding more checks

--- src/validators/default-value-validator.ts
+++ src/validators/default-value-validator.ts
@@ -8,7 +8,7 @@ export const DefaultFieldValueValidator: FormFieldValidator = {
       const valuesToCheck = Array.isArray(value) ? value : [value];
       // check whether value exists in answers
       if (
-        !valuesToCheck.some((val: string) => field.questionOptions.answers?.find((answer) => answer.concept === val))
+        !valuesToCheck.every((val: string) => field.questionOptions.answers?.find((answer) => answer.concept === val))
       ) {
         return [
           { resultType: 'error', errCode: 'invalid.defaultValue', message: 'Value not found in coded answers list' },
