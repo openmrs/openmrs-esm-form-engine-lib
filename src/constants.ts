1b7546a9d539658c5102827ee62c2d4d9140dedc
CynthiaKamau <cynthiakamau54@gmail.com>
2024-05-20 22:18:05 +0300

    O3-3166Use a custom encounter representation post encounter submission

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ export const ConceptTrue = 'cf82933b-3f3f-45e7-a5ab-5d31aaee3da3';
 export const ConceptFalse = '488b58ff-64f5-4f8a-8979-fa79940b1594';
 export const UnspecifiedValue = 'VALUE_UNSPECIFIED';
 export const encounterRepresentation =
-  'custom:(uuid,encounterDatetime,encounterType,location:(uuid,name),' +
-  'patient:(uuid,display),encounterProviders:(uuid,provider:(uuid,name)),' +
+  'custom:(uuid,encounterDatetime,encounterType:(uuid,name,description),location:(uuid,name),' +
+  'patient:(uuid,display),encounterProviders:(uuid,provider:(uuid,name),encounterRole:(uuid,name)),' +
   'orders:(uuid,display,concept:(uuid,display),voided),' +
   'obs:(uuid,obsDatetime,voided,groupMembers,formFieldNamespace,formFieldPath,concept:(uuid,name:(uuid,name)),value:(uuid,name:(uuid,name),' +
   'names:(uuid,conceptNameType,name))))';
