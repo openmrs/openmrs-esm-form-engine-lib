f453c0564d6bc6d14e120da4056df9dd8cb1eaa4
gitcliff <gitacliff48@gmail.com>
2024-06-03 08:38:05 +0300

    encounterProvider:add tests in a single test suite

--- src/submission-handlers/encounterProviderHandler.test.ts
+++ src/submission-handlers/encounterProviderHandler.test.ts
@@ -24,24 +24,6 @@ const encounterContext: EncounterContext = {
   setEncounterRole: jest.fn,
 };
 
-const encounterContext2: EncounterContext = {
-  patient: {
-    id: '833eb896-c1f0-11eb-8529-0242ac130003',
-  },
-  location: {
-    uuid: '81e6e516-c1f0-11eb-8529-0242ac130003',
-  },
-  sessionMode: 'enter',
-  encounterDate: new Date(2020, 11, 29),
-  setEncounterDate: (value) => {},
-  encounterProvider: '2c95f6f5-788e-4e73-9079-5626911231fa',
-  setEncounterProvider: jest.fn,
-  setEncounterLocation: jest.fn,
-  encounterRole: '6d95f6f5-788e-4e73-9079-5626911231f4',
-  setEncounterRole: jest.fn,
-  encounter: undefined,
-};
-
 const allFormFieldsMock: FormField[] = [
   {
     label: 'Field 1',
@@ -65,114 +47,108 @@ const allFormFieldsMock: FormField[] = [
   },
 ];
 
-describe('EncounterProviderHandler - handleFieldSubmission', () => {
-  // new submission (enter mode)
-  it('should handle encounter provider submission for ui-select-extended input', () => {
-    // setup
-    const field: FormField = {
-      label: 'Encounter provider',
-      type: 'encounterProvider',
-      required: false,
-      id: 'encounterProvider',
-      questionOptions: {
-        rendering: 'ui-select-extended',
-      },
-      validators: [],
-    };
-    // replay
-    const provider = EncounterProviderHandler.handleFieldSubmission(field, 'New Provider', encounterContext);
-    // verify
-    expect(provider).toEqual('New Provider');
+describe('EncounterProviderHandler', () => {
+  describe('handleFieldSubmission', () => {
+    // new submission (enter mode)
+    it('should handle encounter provider submission', () => {
+      // setup
+      const field: FormField = {
+        label: 'Encounter provider',
+        type: 'encounterProvider',
+        required: false,
+        id: 'encounterProvider',
+        questionOptions: {
+          rendering: 'ui-select-extended',
+        },
+        validators: [],
+      };
+      // replay
+      const provider = EncounterProviderHandler.handleFieldSubmission(field, 'New Provider', encounterContext);
+      // verify
+      expect(provider).toEqual('New Provider');
+    });
   });
-});
 
-describe('EncounterProviderHandler - getInitialValue', () => {
-  // new submission (enter mode)
-  it('should get initial value for ui-select-extended rendering', () => {
-    // setup
-    const field: FormField = {
-      label: 'Encounter provider',
-      type: 'encounterProvider',
-      required: false,
-      id: 'encounterProvider',
-      questionOptions: {
-        rendering: 'ui-select-extended',
-      },
-      validators: [],
-    };
-    const encounterProviders: any = {
-      uuid: 'b6194998-7b44-48f3-a697-f762f337e2fe',
-      provider: {
-        uuid: 'f39e57d8-1185-4199-8567-6f1eeb160f05',
-        name: 'Super User',
-      },
-    };
-    encounterContext.encounter['encounterProviders'].push(encounterProviders);
+  describe('getInitialValue', () => {
+    it('should get initial value for the encounter provider', () => {
+      // setup
+      const field: FormField = {
+        label: 'Encounter provider',
+        type: 'encounterProvider',
+        required: false,
+        id: 'encounterProvider',
+        questionOptions: {
+          rendering: 'ui-select-extended',
+        },
+        validators: [],
+      };
+      const encounterProviders: any = {
+        uuid: 'b6194998-7b44-48f3-a697-f762f337e2fe',
+        provider: {
+          uuid: 'f39e57d8-1185-4199-8567-6f1eeb160f05',
+          name: 'Super User',
+        },
+      };
+      encounterContext.encounter['encounterProviders'].push(encounterProviders);
 
-    // replay
-    const provider = EncounterProviderHandler.getInitialValue(
-      encounterContext.encounter,
-      field,
-      allFormFieldsMock,
-      encounterContext,
-    );
-    // verify
-    expect(provider).toEqual('f39e57d8-1185-4199-8567-6f1eeb160f05');
+      // replay
+      const provider = EncounterProviderHandler.getInitialValue(
+        encounterContext.encounter,
+        field,
+        allFormFieldsMock,
+        encounterContext,
+      );
+      // verify
+      expect(provider).toEqual('f39e57d8-1185-4199-8567-6f1eeb160f05');
+    });
+
+    it('should use encounterProvider from the context object if encounter is undefined', () => {
+      // setup
+      const field: FormField = {
+        label: 'Encounter provider',
+        type: 'encounterProvider',
+        required: false,
+        id: 'encounterProvider',
+        questionOptions: {
+          rendering: 'ui-select-extended',
+        },
+        validators: [],
+      };
+
+      // replay
+      const provider = EncounterProviderHandler.getInitialValue(null, field, allFormFieldsMock, encounterContext);
+      // verify
+      expect(provider).toEqual('2c95f6f5-788e-4e73-9079-5626911231fa');
+    });
   });
-});
 
-describe('EncounterProviderHandler - getInitialValue', () => {
-  it('should use encounterProvider from the context object if encounter is undefined', () => {
-    // setup
-    const field: FormField = {
-      label: 'Encounter provider',
-      type: 'encounterProvider',
-      required: false,
-      id: 'encounterProvider',
-      questionOptions: {
-        rendering: 'ui-select-extended',
-      },
-      validators: [],
-    };
+  describe('getPreviousValue', () => {
+    it('should get previous encounter provider value', () => {
+      // setup
+      const field: FormField = {
+        label: 'Encounter Provider',
+        type: 'encounterProvider',
+        required: false,
+        id: 'encounterProvider',
+        questionOptions: {
+          rendering: 'ui-select-extended',
+        },
+        validators: [],
+      };
 
-    // replay
-    const provider = EncounterProviderHandler.getInitialValue(
-      encounterContext2.encounter,
-      field,
-      allFormFieldsMock,
-      encounterContext,
-    );
-    // verify
-    expect(provider).toEqual('2c95f6f5-788e-4e73-9079-5626911231fa');
-  });
-});
+      const encounterProviders: any = {
+        uuid: 'b6194998-7b44-48f3-a697-f762f337e2fe',
+        provider: {
+          uuid: 'f39e57d8-1185-4199-8567-6f1eeb160f05',
+          name: 'Super User',
+        },
+      };
+      encounterContext.encounter['encounterProviders'].push(encounterProviders);
 
-describe('EncounterProviderHandler - getPreviousValue', () => {
-  it('should get previous value for ui-select-extended rendering', () => {
-    // setup
-    const field: FormField = {
-      label: 'Encounter Provider',
-      type: 'encounterProvider',
-      required: false,
-      id: 'encounterProvider',
-      questionOptions: {
-        rendering: 'ui-select-extended',
-      },
-      validators: [],
-    };
-
-    const encounterProviders: any = {
-      uuid: 'b6194998-7b44-48f3-a697-f762f337e2fe',
-      provider: {
-        uuid: 'f39e57d8-1185-4199-8567-6f1eeb160f05',
-        name: 'Super User',
-      },
-    };
-    encounterContext.encounter['encounterProviders'].push(encounterProviders);
-
-    // replay
-    const provider = EncounterProviderHandler.getPreviousValue(field, encounterContext.encounter, allFormFieldsMock);
-    // verify
-    expect(provider).toEqual({ display: 'Super User', value: 'f39e57d8-1185-4199-8567-6f1eeb160f05' });
+      // replay
+      const provider = EncounterProviderHandler.getPreviousValue(field, encounterContext.encounter, allFormFieldsMock);
+      // verify
+      expect(provider).toEqual({ display: 'Super User', value: 'f39e57d8-1185-4199-8567-6f1eeb160f05' });
+    });
   });
 });
