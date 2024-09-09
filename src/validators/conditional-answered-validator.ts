092c72b35efd424b2ff706e48ce7b4f984297579
WodPachua <molarag21996@gmail.com>
2024-09-09 18:05:27 +0300

    (fix) fixed bug causing error with repeating render component (#379)
    * (fix) fixed bug causing error with repeating render component
    
    * fix failing tests

--- src/validators/conditional-answered-validator.ts
+++ src/validators/conditional-answered-validator.ts
@@ -3,9 +3,9 @@ import { type FormFieldValidator, type FormField } from '../types';
 
 export const conditionalAnsweredValidator: FormFieldValidator = {
   validate: function (field: FormField, value: unknown, config: Record<string, any>) {
-    const { referenceQuestionId, referenceQuestionAnswers, values, fields, message } = config;
+    const { referenceQuestionId, referenceQuestionAnswers, values, formFields, message } = config;
 
-    const referencedField = fields.find((field) => field.id === referenceQuestionId);
+    const referencedField = formFields.find((field) => field.id === referenceQuestionId);
     const referencedFieldValue = values[referencedField.id];
 
     if (!isEmpty(value) && !referenceQuestionAnswers.includes(referencedFieldValue)) {
