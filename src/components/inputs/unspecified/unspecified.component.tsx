790f4af4cddfeaa04473cb4ce3e9484ac5069e0c
Samuel Male <samuelsmalek@gmail.com>
2024-10-15 08:51:07 +0300

    (fix) O3-4064 - Enhance Support for Marking Fields as Unspecified (#412)
    * Fix support of marking fields as unspecified
    
    * fixup

--- src/components/inputs/unspecified/unspecified.component.tsx
+++ src/components/inputs/unspecified/unspecified.component.tsx
@@ -7,7 +7,7 @@ import { isTrue } from '../../../utils/boolean-utils';
 
 import styles from './unspecified.scss';
 import { useFormProviderContext } from '../../../provider/form-provider';
-import { isViewMode } from '../../../utils/common-utils';
+import { clearSubmission, isViewMode } from '../../../utils/common-utils';
 
 interface UnspecifiedFieldProps {
   field: FormField;
@@ -29,25 +29,28 @@ const UnspecifiedField: React.FC<UnspecifiedFieldProps> = ({ field, fieldValue, 
   }, []);
 
   useEffect(() => {
-    if (field.meta.submission?.unspecified && field.meta.submission.newValue) {
+    if (field.meta.submission?.unspecified && (field.meta.submission.newValue || !isEmpty(fieldValue))) {
       setIsUnspecified(false);
       field.meta.submission.unspecified = false;
       updateFormField(field);
     }
-  }, [field.meta?.submission]);
+  }, [field.meta?.submission, fieldValue]);
 
   const handleOnChange = useCallback(
     (value) => {
       const rendering = field.questionOptions.rendering;
       if (value.target.checked) {
-        const emptyValue = rendering === 'checkbox' ? [] : '';
-        field.meta.submission = { ...field.meta.submission, unspecified: true };
-        updateFormField({ ...field });
         setIsUnspecified(true);
+        const emptyValue = rendering === 'checkbox' ? [] : '';
+        clearSubmission(field);
+        field.meta.submission.unspecified = true;
+        updateFormField(field);
         setFieldValue(emptyValue);
         onAfterChange(emptyValue);
       } else {
         setIsUnspecified(false);
+        field.meta.submission.unspecified = false;
+        updateFormField(field);
       }
     },
     [field.questionOptions.rendering],
