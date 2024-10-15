ea9b8fe836728463cf229ef2e1ca8abf58ec32de
Twiineenock <twiineenockfox@gmail.com>
2024-10-16 02:12:33 +0300

    remove rehype-raw and remark-gfm

--- src/components/inputs/markdown/markdown-wrapper.component.tsx
+++ src/components/inputs/markdown/markdown-wrapper.component.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
 
 const MarkdownWrapper: React.FC<{ markdown: string | string[] }> = ({ markdown }) => {
   return (
     <ReactMarkdown
       children={Array.isArray(markdown) ? markdown.join('\n') : markdown}
       unwrapDisallowed={true}
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
       allowedElements={[
         'h1',
         'h2',
@@ -20,13 +16,6 @@ const MarkdownWrapper: React.FC<{ markdown: string | string[] }> = ({ markdown }
         'p',
         'strong',
         'em',
-        'ul',
-        'ol',
-        'li',
-        'input',
-        'sup',
-        'sub',
-        'del',
       ]}
     />
   );
