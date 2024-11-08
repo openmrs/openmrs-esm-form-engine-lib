df1baf276055726124a4d99b72241f36c22461c8
wodpachua <molarag21996@gmail.com>
2024-11-08 03:06:08 +0300

    rename test file

---
+++ src/components/repeat/repeat.test.tsx
@@ -0,0 +1,29 @@
+import { updateFieldIdInExpression } from './helpers';
+
+describe('RepeatingFieldComponent - handleExpressionFieldIdUpdate', () => {
+  it('Should handle update of expression with ids in repeat group', () => {
+    const expression =
+      "infantStatus !== '151849AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' && infantStatus !== '154223AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'";
+    const fieldIds = ['birthDate', 'infantStatus', 'deathDate'];
+    const index = 2;
+
+    const updatedExpression = updateFieldIdInExpression(expression, index, fieldIds);
+
+    expect(updatedExpression).toEqual(
+      "infantStatus_2 !== '151849AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' && infantStatus_2 !== '154223AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'",
+    );
+  });
+
+  it('Should handle update of expression with ids not in repeat group', () => {
+    const expression =
+      "myValue > today() || myValue <= '1/1/1890' || myValue > useFieldValue('visit_date') || myValue < useFieldValue('visit_date')";
+    const fieldIds = ['birthDate', 'infantStatus', 'deathDate'];
+    const index = 1;
+
+    const updatedExpression = updateFieldIdInExpression(expression, index, fieldIds);
+
+    expect(updatedExpression).toEqual(
+      "myValue > today() || myValue <= '1/1/1890' || myValue > useFieldValue('visit_date') || myValue < useFieldValue('visit_date')",
+    );
+  });
+});
