c4536eb26c1af3e6ffc0d5d80326cf5074baf33d
olewandowski1 <olewandowski@soldevelo.com>
2025-07-29 15:29:51 +0200

    (feat) Hide interactive elements when embedded-view mode applied

--- src/components/inputs/workspace-launcher/workspace-launcher.component.tsx
+++ src/components/inputs/workspace-launcher/workspace-launcher.component.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { showSnackbar } from '@openmrs/esm-framework';
 import { useLaunchWorkspaceRequiringVisit } from '@openmrs/esm-patient-common-lib';
 import { Button } from '@carbon/react';
+import { useFormProviderContext } from '../../../provider/form-provider';
 import { type FormFieldInputProps } from '../../../types';
 import { isTrue } from '../../../utils/boolean-utils';
 import styles from './workspace-launcher.scss';
@@ -10,6 +11,7 @@ import styles from './workspace-launcher.scss';
 const WorkspaceLauncher: React.FC<FormFieldInputProps> = ({ field }) => {
   const { t } = useTranslation();
   const launchWorkspace = useLaunchWorkspaceRequiringVisit(field.questionOptions?.workspaceName);
+  const { sessionMode } = useFormProviderContext();
 
   const handleLaunchWorkspace = () => {
     if (!launchWorkspace) {
@@ -23,17 +25,19 @@ const WorkspaceLauncher: React.FC<FormFieldInputProps> = ({ field }) => {
     launchWorkspace();
   };
 
+  if (field.isHidden || sessionMode === 'embedded-view') {
+    return null;
+  }
+
   return (
-    !field.isHidden && (
-      <div>
-        <div className={styles.label}>{t(field.label)}</div>
-        <div className={styles.workspaceButton}>
-          <Button disabled={isTrue(field.readonly)} onClick={handleLaunchWorkspace}>
-            {t(field.questionOptions?.buttonLabel) ?? t('launchWorkspace', 'Launch Workspace')}
-          </Button>
-        </div>
+    <div>
+      <div className={styles.label}>{t(field.label)}</div>
+      <div className={styles.workspaceButton}>
+        <Button disabled={isTrue(field.readonly)} onClick={handleLaunchWorkspace}>
+          {t(field.questionOptions?.buttonLabel) ?? t('launchWorkspace', 'Launch Workspace')}
+        </Button>
       </div>
-    )
+    </div>
   );
 };
 
