ee82c2b767c6efbd9ef353120eee2c8073dbd197
olewandowski1 <olewandowski@soldevelo.com>
2025-08-20 15:22:09 +0200

    OB-4959: allow to hide unanswered questions in read only forms

--- src/hooks/useEvaluateFormFieldExpressions.ts
+++ src/hooks/useEvaluateFormFieldExpressions.ts
@@ -6,6 +6,7 @@ import { isTrue } from '../utils/boolean-utils';
 import { isEmpty } from '../validators/form-validator';
 import { type QuestionAnswerOption } from '../types/schema';
 import { updateFormSectionReferences } from '../utils/common-utils';
+import { useConfig } from '@openmrs/esm-framework';
 
 export const useEvaluateFormFieldExpressions = (
   formValues: Record<string, any>,
@@ -14,6 +15,7 @@ export const useEvaluateFormFieldExpressions = (
   const { formFields, patient, sessionMode } = factoryContext;
   const [evaluatedFormJson, setEvaluatedFormJson] = useState(factoryContext.formJson);
   const [evaluatedPagesVisibility, setEvaluatedPagesVisibility] = useState(false);
+  const { hideUnansweredQuestionsInReadonlyForms } = useConfig();
 
   const evaluatedFields = useMemo(() => {
     return formFields?.map((field) => {
@@ -38,7 +40,8 @@ export const useEvaluateFormFieldExpressions = (
           });
         }
       } else {
-        field.isHidden = false;
+        field.isHidden =
+          hideUnansweredQuestionsInReadonlyForms && sessionMode === 'embedded-view' && !hasAnswer(field, formValues);
       }
       // evaluate required
       if (typeof field.required === 'object' && field.required.type === 'conditionalRequired') {
@@ -148,4 +151,9 @@ export const useEvaluateFormFieldExpressions = (
 
 function isNotBooleanString(str: string) {
   return str !== 'true' && str !== 'false';
+}
+
+function hasAnswer(field: any, formValues: Record<string, any>): boolean {
+  const value = formValues[field.id];
+  return !isEmpty(value);
 }
