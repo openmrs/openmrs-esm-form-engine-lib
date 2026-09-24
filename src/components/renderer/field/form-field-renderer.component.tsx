72dda8e32417b1361fdceabdbcae6b49c71ec5c5
Josephkagimu1 <88326256+Josephkagimu1@users.noreply.github.com>
2026-09-24 20:29:02 +0100

    Update src/components/renderer/field/form-field-renderer.component.tsx
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- src/components/renderer/field/form-field-renderer.component.tsx
+++ src/components/renderer/field/form-field-renderer.component.tsx
@@ -97,7 +97,6 @@ export const FormFieldRenderer = ({ fieldId, valueAdapter, repeatOptions }: Form
   }, []);
 
   useEffect(() => {
-    const { isDirty, isTouched } = getFieldState(field.id);
     const { submission, unspecified } = field.meta;
     const { calculate, defaultValue } = field.questionOptions;
     if (
